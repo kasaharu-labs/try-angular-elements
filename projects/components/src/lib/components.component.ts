d0bbfa1fae7fdac5c30d784a88dcecb196771b1e
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-27 10:27:13 +0900

    yarn ng g library components

---
+++ projects/components/src/lib/components.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'lib-components',
+  template: `
+    <p>
+      components works!
+    </p>
+  `,
+  styles: [
+  ],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class ComponentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
