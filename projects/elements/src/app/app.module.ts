94ae16719b2cfd5a04568ba7d1dd8b34c70d5318
Wataru.Kasahara <kasahara1019@gmail.com>
2020-10-28 09:30:18 +0900

    feat: Custom Element 化のセットアップ

--- projects/elements/src/app/app.module.ts
+++ projects/elements/src/app/app.module.ts
@@ -1,10 +1,18 @@
+import { Injector, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { ComponentsComponent, ComponentsModule } from 'components';
 
 @NgModule({
   declarations: [],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ComponentsModule],
   providers: [],
-  bootstrap: [],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {}
+
+  ngDoBootstrap() {
+    const element = createCustomElement(ComponentsComponent, { injector: this.injector });
+    customElements.define('lib-components', element);
+  }
+}
